eb6bbec218e875e81847e5525717af383ddfdcd8
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2020-01-26 19:09:45 +0530

    gallery code

--- app/src/main/java/com/mywings/thieffacedetector/RegistrationActivity.kt
+++ app/src/main/java/com/mywings/thieffacedetector/RegistrationActivity.kt
@@ -1,12 +1,119 @@
 package com.mywings.thieffacedetector
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_registration.*
 
-class RegistrationActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+
+            } else {
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                    // Marshmallow+
+                    requestPermissions(
+                        arrayOf(
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ),
+                        PERMISSION
+                    )
+                } else {
+                    //below Marshmallow
+                }
+
+            }
+        }
+
+        imgPhoto.setOnClickListener {
+            showMenu(it)
+        }
+    }
+
+    private fun showMenu(v: View) {
+        PopupMenu(this, v).apply {
+            setOnMenuItemClickListener(this@RegistrationActivity)
+            inflate(R.menu.camera_menu)
+            show()
+        }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menu_camera -> {
+                startCamera()
+                true
+            }
+            R.id.menu_gallery -> {
+                startGallery()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE)
+    }
+
+    private fun startCamera() {
+        val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST)
+    }
+
+    companion object {
+        const val PERMISSION = 1001
+        const val CAMERA_PIC_REQUEST = 1002
+        const val SELECT_IMAGE = 1003
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                when (requestCode) {
+                    CAMERA_PIC_REQUEST -> {
+                        val image = data!!.extras?.get("data") as Bitmap
+                        imgPhoto.setImageBitmap(image)
+                    }
+                    SELECT_IMAGE -> {
+                        val image =
+                            MediaStore.Images.Media.getBitmap(contentResolver, data?.data)
+                        imgPhoto.setImageBitmap(image)
+                    }
+                }
+            }
+        }
     }
 }
